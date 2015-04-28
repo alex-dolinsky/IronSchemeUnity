ce8998f31ba95e3614115cfc96317dddbd3bf609
= <alex.dolinsky.1984@gmail.com>
2015-04-28 17:29:11 +0200

    Added back- / forth-travelling line recall. Added paste. TAB - paste from system buffer. UP ARROW - recall back. DOWN ARROW - recall forth.

--- Editor/SchemeReplWindow.cs
+++ Editor/SchemeReplWindow.cs
@@ -4,6 +4,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
+using System.Reflection;
+
+public class ClipboardHelper {
+	private static PropertyInfo m_systemCopyBufferProperty = null;
+	private static PropertyInfo GetSystemCopyBufferProperty() {
+	    if (m_systemCopyBufferProperty == null) {
+	        Type T = typeof (GUIUtility);
+	        m_systemCopyBufferProperty = T.GetProperty ("systemCopyBuffer", BindingFlags.Static | BindingFlags.NonPublic);
+	        if (m_systemCopyBufferProperty == null) { throw new Exception ("Can't access internal member 'GUIUtility.systemCopyBuffer' it may have been removed / renamed"); }
+	    }
+	    return m_systemCopyBufferProperty;
+	}
+	public static string clipBoard {
+	    get { PropertyInfo P = GetSystemCopyBufferProperty ();
+	          return (string) P.GetValue (null, null); }
+	    set { PropertyInfo P = GetSystemCopyBufferProperty ();
+	          P.SetValue (null, value, null); }
+	}
+}
+
 public class SchemeReplWindow : EditorWindow {
 	
 	[MenuItem("Window/Repl")]
@@ -15,6 +35,9 @@ public class SchemeReplWindow : EditorWindow {
 	// Repl
 	List<string> replHistory = new List<string>();
 	string currentForm = "";
+	List<string> lineHistory = new List<string>();
+	int lineHistoryIdx = 0;
+	int lineHistorySize = 0;
 	string currentLine = "";
 	
 	bool parensMatch(string form) {
@@ -52,13 +75,43 @@ public class SchemeReplWindow : EditorWindow {
 		GUI.SetNextControlName("repl-input");
 		currentLine = GUILayout.TextField(currentLine);
 		
+		if (Event.current.isKey && Event.current.keyCode == KeyCode.Tab) {
+			currentLine = ClipboardHelper.clipBoard;
+		}
+
+		if (Event.current.isKey && Event.current.keyCode == KeyCode.UpArrow) {
+			lineHistoryIdx--;
+			if (lineHistoryIdx >= 0 && lineHistoryIdx < lineHistorySize) {
+				currentLine = lineHistory[lineHistoryIdx];
+			} else {
+				currentLine = "";
+				lineHistoryIdx = lineHistorySize;
+			}
+		}
+
+		if (Event.current.isKey && Event.current.keyCode == KeyCode.DownArrow) {
+			lineHistoryIdx++;
+			if (lineHistoryIdx >= 0 && lineHistoryIdx < lineHistorySize) {
+				currentLine = lineHistory[lineHistoryIdx];
+			} else {
+				currentLine = "";
+				lineHistoryIdx = -1;
+			}
+		}
+
 		if (Event.current.isKey && Event.current.keyCode == KeyCode.Return) {
 			replHistory.Add(currentLine);
 			currentForm = currentForm + "\n" + currentLine;
+
+			lineHistory.Add (currentLine);
+			lineHistorySize++;
+			lineHistoryIdx++;
+
 			currentLine = "";
-			if (parensMatch(currentForm) && currentForm != "") {
+			if (currentForm != "" && parensMatch(currentForm)) {
 				replHistory.Add(">" + Eval(currentForm));
 				currentForm = "";
+				lineHistoryIdx = lineHistorySize;
 			}
 			scroll_position.y = 100000;
 		}
